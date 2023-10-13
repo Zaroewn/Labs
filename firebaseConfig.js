630ab6f257bc21950b8547c47c273adcc1c18ce7
zaroewn <maxime.lamotte@outlook.com>
2023-10-13 10:33:53 +0200

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACdB1wyOzGq6GqAkNAoOU9qFWsbtUF5y0",
+  authDomain: "maxime-lamotte.firebaseapp.com",
+  projectId: "maxime-lamotte",
+  storageBucket: "maxime-lamotte.appspot.com",
+  messagingSenderId: "858405143374",
+  appId: "1:858405143374:web:3a4e8144fc4eaa2388941f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
